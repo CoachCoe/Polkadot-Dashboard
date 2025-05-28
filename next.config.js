f24bcabaccc9d1d8f59c8f3e809a6921a8ff0cb7
CoachCoe <shawn.coe@me.com>
2025-05-27 21:31:12 -0400

    fix(build): Configure Next.js for static export

--- next.config.js
+++ next.config.js
@@ -59,6 +59,7 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/Polkadot-Dashboard' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/Polkadot-Dashboard' : '',
+  output: 'export',
 }
 
 module.exports = nextConfig 
